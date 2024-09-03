52ff21fad6f0959b123f2ec90a991d85e9e0e0e3
SamuelKane2 <slkane3@icloud.com>
2024-09-03 13:16:36 -0400

    sdf

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -6,3 +6,5 @@ extends Area2D
 func _on_body_entered(body):
 	game_manager.add_point()
 	animation_player.play("pickup")
+
+@onready var score_label = $"../../Player/Camera2D/ScoreLabel"
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,9 @@
 extends Node
 
-var score = 0
+#var score = 0
 
-@onready var score_label: Label = $ScoreLabel
+#@onready var score_label: Label = $ScoreLabel
 
-func add_point():
-	score += 1
-	score_label.text = "Score: " + str(score)
+#func add_point():
+	#score += 1
+	#score_label.text = "Score: " + str(score)
