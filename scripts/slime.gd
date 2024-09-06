bb1d019e6320179d15d832ef00bfed840b33c057
logandav3707 <ldavis@ftstudent.org>
2024-09-06 11:59:55 -0400

    not flying
    I made the slimes not fly off in the distance

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-const SPEED = 60
+const SPEED = 30
 
 var direction = 1
 
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_down_right = $RayCastDownRight
+@onready var ray_cast_down_left = $RayCastDownLeft
 @onready var animated_sprite = $AnimatedSprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,3 +19,9 @@ func _process(delta):
 		direction = 1
 		animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
+	if not ray_cast_down_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	if not ray_cast_down_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
