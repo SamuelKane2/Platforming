f3e6021e6b72f0b8342ac52c628a65a253a69856
SamuelKane2 <slkane3@icloud.com>
2024-09-10 13:11:42 -0400

    FIXKJ;LKDJFASDF

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,14 +1,6 @@
 extends Area2D
 
-@onready var timer = $Timer
-
 func _on_body_entered(body):
-	print("You died!")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
-	
-
-func _on_timer_timeout():
-	Engine.time_scale = 1.0
-	get_tree().reload_current_scene()
+	$DeathSFX.play()
+	if body is Player:
+		body.do_death()
